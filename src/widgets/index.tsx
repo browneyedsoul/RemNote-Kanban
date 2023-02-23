a2bb6b35bfa06dfa37f94ef29d0e9a6aa7264935
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-23 11:07:04 +0900

    Fix for mobile rendering

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -5,25 +5,25 @@ export const KANBAN = "powerup_kanban";
 let Kanban: string;
 
 async function onActivate(plugin: ReactRNPlugin) {
-  // try {
-  //   await fetch("snippet.css")
-  //     .then((response) => response.text())
-  //     .then((text) => {
-  //       Kanban = text;
-  //       console.dir("Kanban imported!");
-  //     })
-  //     .catch((error) => console.error(error));
-  //   await plugin.app.registerCSS("Kanban", Kanban);
-  // } catch (error) {
-  //   await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-Kanban/main/src/snippet.css")
-  //     .then((response) => response.text())
-  //     .then((text) => {
-  //       Kanban = text;
-  //       console.log("Kanban imported from cdn!");
-  //     })
-  //     .catch((error) => console.error(error));
-  //   await plugin.app.registerCSS("Kanban", Kanban);
-  // }
+  try {
+    await fetch("snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        Kanban = text;
+        console.dir("Kanban imported!");
+      })
+      .catch((error) => console.error(error));
+    await plugin.app.registerCSS("Kanban", Kanban);
+  } catch (error) {
+    await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-Kanban/main/src/snippet.css")
+      .then((response) => response.text())
+      .then((text) => {
+        Kanban = text;
+        console.log("Kanban imported from cdn!");
+      })
+      .catch((error) => console.error(error));
+    await plugin.app.registerCSS("Kanban", Kanban);
+  }
   await plugin.app.registerPowerup("Kanban", KANBAN, "A Power-up Rem for making css-kanban", { slots: [] });
   await plugin.app.registerCommand({
     id: "kanban",
